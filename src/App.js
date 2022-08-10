66b1b6fa6eacbbf8f7f19bad74dbde89a2e2187b
Chris <ryanadamz1821@gmail.com>
2022-08-10 17:00:11 +0000

    This is a react todo app

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+// importing components
+import Form from "./components/form";
+function App() {
+  return (
+    <>
+      <header>
+        EliBaron's Todo  App
+      </header>
+      <Form />
+    </>
+  );
+}
+
+export default App;
